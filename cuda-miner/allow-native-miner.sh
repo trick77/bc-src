658de95daec0daea3cb14f93ca12bc9cc3ae7824
trick77 <jan@trick77.com>
2020-04-03 21:45:33 +0200

    Disabling the disabling.

--- cuda-miner/allow-native-miner.sh
+++ cuda-miner/allow-native-miner.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 sed -i 's/if[[:space:]]\+(false[[:space:]]\+||[[:space:]]\+BC_RUST_MINER)[[:space:]]\+{/if (BC_RUST_MINER) {/g' ../lib/mining/officer.js
-sed -i 's/if[[:space:]]\+(false[[:space:]]\+||[[:space:]]\+BC_RUST_MINER)[[:space:]]\+{/if (BC_RUST_MINER) {/g' ../lib/engine/index.js
