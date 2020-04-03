b7b461b7a5f4c53dc5c809e5b06fe28e977d0885
trick77 <jan@trick77.com>
2020-04-03 20:39:54 +0200

    Major master branch cleanup.

--- build-cuda.sh
+++
@@ -1,5 +0,0 @@
-. ./buildenv_cuda
-pushd bc-miner-cuda/src/
-make -f ../Makefile
-cp miner ../../cuda_miner
-popd
--- keep_alive_miner_cuda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH="${HOME}/grpc/build:$LD_LIBRARY_PATH"
-while true; do
-    logname=miner_`date +%s`.log
-    timeout -s 9 60m ./cuda_miner >& $logname
-    gzip $logname
-done
