1df3cc4b375058367ff1aa77137fd250a87f6385
trick77 <jan@trick77.com>
2020-04-05 17:47:18 +0200

    Added named volume for blockchain db.

--- cuda-miner/docker/start-gpu.sh
+++ cuda-miner/docker/start-gpu.sh
@@ -2,7 +2,7 @@
 image="trick77/bcnode-gpu:latest"
 
 echo "Check the following output if Docker has access to one or more GPUs:"
-docker run --rm --gpus all nvidia/cuda:10.0-base nvidia-smi
+docker run --rm --gpus all nvidia/cuda:10.2-base nvidia-smi
 
 if [ -z "${BC_MINER_KEY}" ]; then
   echo
@@ -22,7 +22,7 @@ fi
 docker run --rm --name bcnode \
 --memory-reservation="6900m" \
 --gpus all \
--p 3000:3000 -p 16060:16060/tcp -p 16060:16060/udp -p 16061:16061/tcp -p 16061:16061/udp -p 36061:36061/tcp -p 36061:36061/udp -p 36060:36060/tcp -p 36060:36060/udp  -d \
+-p 3000:3000 -p 16060:16060/tcp -p 16060:16060/udp -p 16061:16061/tcp -p 16061:16061/udp -p 36061:36061/tcp -p 36061:36061/udp -p 36060:36060/tcp -p 36060:36060/udp -d \
 -e BC_MINER_KEY="${BC_MINER_KEY}" \
 -e BC_NETWORK="main" \
 -e BC_FORCE_MINE=true \
@@ -31,5 +31,6 @@ docker run --rm --name bcnode \
 -e BC_GRPC_RUST_MINER_PORT=50052 \
 -e BC_RUST_MINER=true \
 -e NODE_OPTIONS=--max_old_space_size=6096 \
+--mount source=db,target=/bc/_data \
 ${image} \
 start --rovers --rpc --ws --ui --node --scookie "${BC_SCOOKIE}" 2>&1
