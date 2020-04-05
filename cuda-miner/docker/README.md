077753830c0a3bc10f66ddc8668e639d948d5479
trick77 <jan@trick77.com>
2020-04-05 19:22:26 +0200

    Some docs.

---
+++ cuda-miner/docker/README.md
@@ -0,0 +1,12 @@
+# README.md
+
+Mining on Block Collider with LG's CUDA RPC miner in a Docker container.
+
+In a nutshell:
+1. Install Nvidia's CUDA Drivers
+2. Install Docker
+3. Install https://github.com/NVIDIA/nvidia-docker
+4. Build the Docker image locally (don't rely on updated Dockerhub images)
+5. Start the image with the provided script
+
+
--- cuda-miner/docker/docker-entrypoint.sh
+++ cuda-miner/docker/docker-entrypoint.sh
@@ -10,9 +10,9 @@ echo "Firing up the CUDA miner first..."
 sleep 1
 
 echo "Patching sources on the fly to enable RPC miner..."
-sed -i 's/if[[:space:]]\+(false[[:space:]]\+||[[:space:]]\+BC_RUST_MINER)[[:space:]]\+{/if (BC_RUST_MINER) {/g' ../lib/mining/officer.js
+sed -i 's/if[[:space:]]\+(false[[:space:]]\+||[[:space:]]\+BC_RUST_MINER)[[:space:]]\+{/if (BC_RUST_MINER) {/g' ./lib/mining/officer.js
 
 echo "Starting bcnode..."
 
-./bin/cli $@
+exec "./bin/cli" $@
 
