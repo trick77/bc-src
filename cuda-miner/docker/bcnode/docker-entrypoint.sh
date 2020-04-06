a670c48a196d3181d328995e1384fdc6735b5b90
trick77 <jan@trick77.com>
2020-04-07 00:07:30 +0200

    Fixed monkey-patch location.

--- cuda-miner/docker/bcnode/docker-entrypoint.sh
+++ cuda-miner/docker/bcnode/docker-entrypoint.sh
@@ -6,7 +6,7 @@ sed -i 's/if[[:space:]]\+(false[[:space:]]\+||[[:space:]]\+BC_RUST_MINER)[[:spac
 sed -i 's/BC_RUST_MINER/BC_RPC_MINER/g' ./lib/mining/officer.js
 
 echo "*** Applying some more monkey patching..."
-perl -0777pe 's/if \(response\.getResult\(\) === MinerResponseResult\.CANCELED\) \{.*?\}/`cat monkey-patch`/se' ./lib/mining/officer.js
+perl -0777pe 's/if \(response\.getResult\(\) === MinerResponseResult\.CANCELED\) \{.*?\}/`cat /monkey-patch`/se' ./lib/mining/officer.js
 
 echo "*** Applying patch to override the RPC miner's address..."
 sed -i "s/const[[:space:]]\+GRPC_MINER_URL[[:space:]]\+=.*/const GRPC_MINER_URL = 'gpuminer:50052'/g" ./lib/rpc/client.js
